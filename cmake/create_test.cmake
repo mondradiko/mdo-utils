281628770e09e92d93bca8e8c438e2051fc6a1d8
marceline-cramer <cramermarceline@gmail.com>
2021-05-27 04:59:41 -0600

    Prefix create_test test executables with test-

--- cmake/create_test.cmake
+++ cmake/create_test.cmake
@@ -9,7 +9,7 @@ if (NOT CRITERION_FOUND)
 endif ()
 
 function (create_test TEST_LIB TEST_NAME TEST_PATH)
-  set (TEST_EXECUTABLE ${TEST_NAME})
+  set (TEST_EXECUTABLE test-${TEST_NAME})
   add_executable (${TEST_EXECUTABLE} ${TEST_PATH})
   target_link_libraries (${TEST_EXECUTABLE} ${TEST_LIB} ${CRITERION_LIBRARIES})
   add_test (NAME ${TEST_NAME} COMMAND ${TEST_EXECUTABLE} --verbose)
