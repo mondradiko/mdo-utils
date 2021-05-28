46bcc2f35472f866d13f7b38be4e93bc126ad904
marceline-cramer <cramermarceline@gmail.com>
2021-05-27 22:36:24 -0600

    mondradiko_create_test() links to cmocka instead of criterion

--- cmake/mondradiko_create_test.cmake
+++ cmake/mondradiko_create_test.cmake
@@ -2,15 +2,11 @@
 # SPDX-License-Identifier: MIT
 
 include (CTest)
-
-if (NOT CRITERION_FOUND)
-  include (FindPkgConfig)
-  pkg_check_modules (CRITERION criterion>=2.0)
-endif ()
+find_package (cmocka REQUIRED)
 
 function (mondradiko_create_test TEST_LIB TEST_NAME TEST_PATH)
   set (TEST_EXECUTABLE test-${TEST_NAME})
   add_executable (${TEST_EXECUTABLE} ${TEST_PATH})
-  target_link_libraries (${TEST_EXECUTABLE} ${TEST_LIB} ${CRITERION_LIBRARIES})
-  add_test (NAME ${TEST_NAME} COMMAND ${TEST_EXECUTABLE} --verbose)
+  target_link_libraries (${TEST_EXECUTABLE} ${TEST_LIB} cmocka)
+  add_test (NAME ${TEST_NAME} COMMAND ${TEST_EXECUTABLE})
 endfunction ()
