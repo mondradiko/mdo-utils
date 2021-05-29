1ff322d893a5e61f4c33f560e108a6d725315891
marceline-cramer <cramermarceline@gmail.com>
2021-05-28 17:07:20 -0700

    Fix find_library(cmocka)

--- cmake/mondradiko_create_test.cmake
+++ cmake/mondradiko_create_test.cmake
@@ -2,11 +2,11 @@
 # SPDX-License-Identifier: MIT
 
 include (CTest)
-find_package (cmocka REQUIRED)
+find_package (cmocka CONFIG REQUIRED)
 
 function (mondradiko_create_test TEST_LIB TEST_NAME TEST_PATH)
   set (TEST_EXECUTABLE test-${TEST_NAME})
   add_executable (${TEST_EXECUTABLE} ${TEST_PATH})
-  target_link_libraries (${TEST_EXECUTABLE} ${TEST_LIB} cmocka)
+  target_link_libraries (${TEST_EXECUTABLE} ${TEST_LIB} cmocka::cmocka)
   add_test (NAME ${TEST_NAME} COMMAND ${TEST_EXECUTABLE})
 endfunction ()
